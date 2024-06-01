432b309bf4f1b3c6f36734d2b8c465d015be6630
molni9 <114047164+molni9@users.noreply.github.com>
2024-06-01 23:33:00 +0500

    dev

---
+++ src/main/java/com/example/krascala/KrascalaApplication.java
@@ -0,0 +1,12 @@
+package com.example.krascala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrascalaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KrascalaApplication.class, args);
+    }
+}
